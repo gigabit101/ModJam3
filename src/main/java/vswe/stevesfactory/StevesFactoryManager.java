f44409dfe57956b2dbf3c643ba380f4e4cbe339e
gigabit101 <thegigabit101@gmail.com>
2016-11-13 20:24:31 +0000

    version bump

--- src/main/java/vswe/stevesfactory/StevesFactoryManager.java
+++ src/main/java/vswe/stevesfactory/StevesFactoryManager.java
@@ -14,7 +14,7 @@ import vswe.stevesfactory.network.FileHelper;
 import vswe.stevesfactory.network.PacketEventHandler;
 import vswe.stevesfactory.proxy.CommonProxy;
 
-@Mod(modid = StevesFactoryManager.MODID, name = "Steve's Factory Manager", version = "1.0.5")
+@Mod(modid = StevesFactoryManager.MODID, name = "Steve's Factory Manager", version = "1.0.6")
 public class StevesFactoryManager
 {
     public static final String MODID = "StevesFactoryManager";
